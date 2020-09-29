318ca8424ef8b69d4aced26df01b86fa7a9a2d36
James Mernin <jamespmernin@gmail.com>
2020-09-29 17:46:05 -0400

    Solved algorithm

--- getIndexToInsert.js
+++ getIndexToInsert.js
@@ -1,7 +1,12 @@
 //INSERT INDEX
 
 const getIndexToInsert = (arr, num) => {
+  arr.push(num);
+  arr.sort((a, b) => {
+    return a - b;
+  });
 
+  return arr.indexOf(num);
 };
 
 console.log(getIndexToInsert([40, 60, 5, 322, 48, 83], 80));
